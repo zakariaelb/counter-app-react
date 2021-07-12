1fd4a0b5b1e169804606ed1ed294bbbca9f1f966
Zakaria ELB <40664115+zakariaelb@users.noreply.github.com>
2021-07-12 16:32:19 +0100

    0.1

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -22,7 +22,7 @@ export default class Counter extends Component {
                     {this.formatCount()}
                 </span>
                 <button className="btn btn-secondary btn-sm ">Increment</button>
-
+                {this.state.tags.length === 0 && "Please create a new tag!" }
                 {this.renderTags()}
 
             </React.Fragment>
