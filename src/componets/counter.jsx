e31ae4390445803cfc2d0374e8147cf6421f7d1f
Zakaria ELB <40664115+zakariaelb@users.noreply.github.com>
2021-07-12 16:26:10 +0100

    0.1

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -9,7 +9,12 @@ export default class Counter extends Component {
         fontsize: 100,
         fontweight: "bold"
     };
-
+    renderTags() {
+        if (this.state.tags.length === 0 ) return <p>There are no tags !</p>
+        return <ul>
+            {this.state.tags.map(tag => <li key={{tag}}>{tag}</li>)}
+        </ul>
+    }
     render() {
         return (
             <React.Fragment>
@@ -17,9 +22,8 @@ export default class Counter extends Component {
                     {this.formatCount()}
                 </span>
                 <button className="btn btn-secondary btn-sm ">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={{tag}}>{tag}</li>)}
-                </ul>
+
+                {this.renderTags()}
 
             </React.Fragment>
         );
