91e8a9f0d049491e5af11c6ef7cffa1359163cdc
Zakaria ELB <40664115+zakariaelb@users.noreply.github.com>
2021-07-12 16:00:06 +0100

    0.1

---
+++ src/componets/counter.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+export default class Counter extends Component {
+    state = {
+        count: 0,
+    };
+    styles = {
+        fontsize: 100,
+        fontweight: "bold"
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <span style={this.styles} className={this.getBadgeClasses()}>
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm ">Increment</button>
+
+            </React.Fragment>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-"
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        //return this.state.count === 0 ? 'Zero' : this.state.count;
+        //return count === 0 ? <h1>Zero</h1> : count;
+        return count === 0 ? 'Zero' : count;
+    };
+}
+//export default Counter;
