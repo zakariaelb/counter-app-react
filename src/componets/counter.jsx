1851185a56c2d067646668c25b1eba29ac19ec4d
Zakaria ELB <40664115+zakariaelb@users.noreply.github.com>
2021-07-12 16:18:46 +0100

    0.1

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 export default class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tags1', 'tags2', 'tags3']
     };
     styles = {
         fontsize: 100,
@@ -16,6 +17,9 @@ export default class Counter extends Component {
                     {this.formatCount()}
                 </span>
                 <button className="btn btn-secondary btn-sm ">Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={{tag}}>{tag}</li>)}
+                </ul>
 
             </React.Fragment>
         );
@@ -23,7 +27,7 @@ export default class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-"
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
     }
 
